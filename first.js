2f9fd3dde56ffde40c747f907fe743b78cb86a89
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-02 20:53:58 +0530

    update

---
+++ first.js
@@ -0,0 +1,16 @@
+// console.log("welcome");
+// document.write("<h1> hieeee </h1>");
+
+// name = prompt("Enter your name");
+// console.log(name);
+
+// num1 = parseInt(prompt("Enter first number"))
+// num2 = parseInt(prompt("Enter second number"))
+
+// console.log(num1 + num2);
+
+// write a program to take integer from the user and find area of circle
+
+let r = parseInt(prompt("Enter radius of circle"))
+let area = 3.14 * r * r
+console.log(area)
