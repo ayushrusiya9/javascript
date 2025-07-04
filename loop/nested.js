49fb3fc7fa91bac25efc3ade788d004385bdab46
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-04 16:28:21 +0530

    update

---
+++ loop/nested.js
@@ -0,0 +1,23 @@
+// for(let a = 2; a <= 3; a++){
+//     for(let b = 1; b <= 10; b++){
+//         console.log(a * b)
+//     }
+// }
+
+// for(let a = 1; a <= 5; a++){
+//     for(let b = 1; b <= 5; b++){
+//         document.write('*')
+//     }
+//     document.write('<br>')
+// }
+
+for(let a = 1; a <= 5; a++){
+    for(let b = 1; b <= 5; b++){
+        if(a == 1 || a == 5 || b == 1 || b == 5){
+            document.write('*')
+        } else {
+            document.write('&nbsp&nbsp')
+        }
+    }
+    document.write('<br>')
+}
