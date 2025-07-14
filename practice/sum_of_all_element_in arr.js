f8d48afe744e80eaaa0c2268dfebfedb7502c6a2
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-14 19:38:42 +0530

    update

---
+++ practice/sum_of_all_element_in arr.js
@@ -0,0 +1,10 @@
+//Given array find the sum of all element 
+let arr = [2,4,6,8,10]
+
+sum = 0 
+
+for(i of arr){
+    sum = sum + i
+}
+
+console.log(sum)
--- string/string.js
+++ string/string.js
@@ -7,9 +7,13 @@ function string_methods(){
     // let str1 = str.replace(/hello/gi, "hey!!!")
     // let str1 = str.split("-")
     // let str1 = str.split(" ")
-    let str1 = str.indexOf("l",0)//jo bhi pehle mila uska index de diya
-    console.log(str)
-    console.log(str1)
+    // let str1 = str.indexOf("l",0)//jo bhi pehle mila uska index de diya
+    // console.log(str)
+    // console.log(str1)
+    let name = "ayush \"okk\" rusiya"
+    console.log(name)
 }
+
+
 
 // ayush()
