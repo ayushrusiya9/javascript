b8dfa649f5a3ef4a8a8678a70cccaf618cdc10a1
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-14 20:02:39 +0530

    update

---
+++ practice/reverse _an_arr.js
@@ -0,0 +1,11 @@
+//reverse the array
+let arr = [10,20,30,40,50]
+let arr_reverse =[]
+
+for(let i = 0; i<=arr.length; i++){
+    if(arr[i] < arr[i+1]){
+        arr1.unshift(i)
+    }
+}
+
+console.log(arr1)
