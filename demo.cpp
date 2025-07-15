8590e0605dca30242ea5bd1e29f1293e678bb365
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-15 09:42:39 +0530

    readme file

---
+++ demo.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string a = "Ayush is \"best\" boy";
+    cout<<a;
+    return 0;
+}
---
+++ demo.java
@@ -0,0 +1,6 @@
+class demo {
+    public static void main(String[] args) {
+        String a = "ayush is the \"best\" boy";
+        System.out.println(a);
+    }
+}
