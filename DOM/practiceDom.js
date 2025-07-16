d32083baaed53d5b8f35263709e710ede96365fd
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-16 09:30:23 +0530

    update

--- DOM/practiceDom.js
+++ DOM/practiceDom.js
@@ -1,4 +1,14 @@
 function changecolor(){
-    let box = document.querySelector(".box")
+    let box = document.querySelector(".text")
     box.style.backgroundColor = "yellow"
+}
+
+const green = ()=>{
+    let greenBtn = document.querySelector(".box")
+    greenBtn.style.backgroundColor = "green" 
+}
+
+const aqua = ()=>{
+    let aquaBtn = document.querySelector(".box")
+    aquaBtn.style.backgroundColor = "aqua" 
 }
