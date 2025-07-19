93c1f026baf5e2e8ca138d23a0c4b1f0bad1e552
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-07-19 19:45:37 +0530

    update

---
+++ functions/practice2.js
@@ -0,0 +1,20 @@
+function checkPrime(n){
+    if(n == 1){
+        return `${n} is a prime number`
+    }
+    if(n == 2){
+        return `${n} is a prime number`
+    }
+    let i = 2;
+    while(i < n){
+        if(n % i == 0 ){
+            return `Given number ${n} is not a prime number`
+        }
+        i++;
+    }
+    return `Given number ${n} is prime number`;
+}
+
+let number = parseInt(prompt("Enter any number"))
+let result = checkPrime(number)
+alert(result)
