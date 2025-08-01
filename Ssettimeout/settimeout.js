d802dafd56e7b327dde832748d3d985227302dce
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-08-01 16:15:51 +0530

    update

--- Ssettimeout/settimeout.js
+++ Ssettimeout/settimeout.js
@@ -1,15 +1,33 @@
 
 
-let okk=()=>{
-    setTimeout(() => {
-        alert("runnn")
-    }, 3000);
+// let okk=()=>{
+//     setTimeout(() => {
+//         alert("runnn")
+//     }, 3000);
+// }
+
+
+// let i = 0
+// let count = ()=>{
+    //     setInterval(()=>{
+        //         console.log(i)
+        //         i = i + 1
+        //     },1000)
+        // } 
+        
+let w = document.querySelector(".watch")
+let c = 0
+let s
+let start = ()=>{
+    s = setInterval(()=>{
+        w.innerHTML = c
+        if (c == 10){
+            stopp()
+        }
+        c++
+    },1000)
 }
 
-let i = 0
-let count = ()=>{
-    setInterval(()=>{
-        console.log(i)
-        i = i + 1
-    },1000)
+let stopp = ()=>{
+    clearInterval(s)
 }
