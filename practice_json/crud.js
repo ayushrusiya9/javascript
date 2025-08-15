c94d90e510dbbe26357898029eed598c2838cfa9
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-08-15 23:50:29 +0530

    update

--- practice_json/crud.js
+++ practice_json/crud.js
@@ -15,7 +15,7 @@ let fetchData = async()=>{
             <td>${e.holder_name}</td>
             <td>${e.seat_number}</td>
             <td>${e.price}</td>
-            <td>Edit</td>
+            <td onclick="openEditForm('${e.id}')">Edit</td>
             <td onclick="del('${e.id}')" >Delete</td>
         `
     })
@@ -52,33 +52,56 @@ let uploadData = ()=>{
     )
 }
 
-let openEditForm = (id)=>{
+let openEditForm = async (id)=>{
     let url = `http://localhost:3000/Ticket/${id}`
 
-    let res = fetch(url)
+    let res = await fetch(url)
 
-    let data = res.json()
+    let data = await res.json()
 
     document.querySelector(".openform").innerHTML += `
          <form id="ticketForm">
         <label for="ticket_id">Ticket ID:</label>
-        <input type="number" id="ticket_id" name="ticket_id" value="${ data.ticket_id}"><br><br>
+        <input type="number" id="edit_ticket_id" name="ticket_id" value="${ data.ticket_id}"><br><br>
 
         <label for="event">Event Name:</label>
-        <input type="text" id="event" name="event" value="${ data.event}"><br><br>
+        <input type="text" id="edit_event" name="event" value="${ data.event}"><br><br>
 
         <label for="holder_name">Holder Name:</label>
-        <input type="text" id="holder_name" name="holder_name" ><br><br>
+        <input type="text" id="edit_holder_name" name="holder_name" value="${ data.holder_name}"><br><br>
 
-        <label for="seat_number">Seat Number:</label>
-        <input type="text" id="seat_number" name="seat_number" required><br><br>
+        <label for="seat_number">Seat Number:</label>git 
+        <input type="text" id="edit_seat_number" name="seat_number" value="${data.seat_number}"><br><br>
 
         <label for="price">Price:</label>
-        <input type="number" id="price" name="price" required><br><br>
+        <input type="number" id="edit_price" name="price" value="${data.price}"><br><br>
 
 
-        <button type="submit" onclick="uploadData()">Submit</button>
+        <button type="submit" onclick="finalEdita('${data.id}')">Submit</button>
     </form>
     `
 
+}
+
+let finalEdita = async (id)=>{
+    let edit_ticketid = document.querySelector("#edit_ticket_id")
+    let editevent = document.querySelector("#edit_event")
+    let editgoldername = document.querySelector("#edit_holder_name")
+    let editseatnum = document.querySelector("#edit_seat_number")
+    let editprice = document.querySelector("#edit_price")
+
+    let url = `http://localhost:3000/Ticket/${id}`
+
+    fetch(url,{method: "PUT",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify({
+            ticket_id:edit_ticketid,
+            event:editevent,
+            holder_name:editgoldername,
+            seat_number:editseatnum,
+            price:editprice
+        })
+    })
 }
