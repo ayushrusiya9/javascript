b5f7e0d89c9a21c1a5c2c0b734ee091caeb16119
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-08-22 21:20:21 +0530

    update

--- first.js
+++ first.js
@@ -11,6 +11,28 @@
 
 // write a program to take integer from the user and find area of circle
 
-let r = parseInt(prompt("Enter radius of circle"))
-let area = 3.14 * r * r
-console.log(area)
+// let r = parseInt(prompt("Enter radius of circle"))
+// let area = 3.14 * r * r
+// console.log(area)
+
+var a = 10
+var a =  20
+a =23
+console.log(a)
+
+let b = 120
+b = 20
+console.log(b)
+
+console.log(typeof(a), b )
+
+// let person = { name: "Aashu", age: 21 };
+// let person = [ 'as', "Aashu", 'age', 21 ];
+let person = "ayush"
+
+for(let i of person){
+    console.log(i)
+}
+
+let x = 5
+console.log(++x,x+2,x++)
