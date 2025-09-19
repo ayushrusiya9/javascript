7acb4ee4977e5e360e793f0e9c69c7cb075f9929
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-09-19 23:06:59 +0530

    update

--- practice/arrow_func.js
+++ practice/arrow_func.js
@@ -1,8 +1,11 @@
 // wa arrow functin to print square a number
-const square = (n)=>{
+// const square = (n)=>{
+//     return n * n
+// }
+
+let square = (n)=>{
     return n * n
 }
-
 let n = 4
 let sqr = square(n)
 console.log(sqr)
