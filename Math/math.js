42957ee199648b60492c6e75221e7889fb717d41
Ayush Rusiya <166496159+ayushrusiya9@users.noreply.github.com>
2026-05-23 23:55:57 +0530

    Update math.js

--- Math/math.js
+++ Math/math.js
@@ -1,3 +1,4 @@
 // console.log(Math.floor(1000+Math.random()*9000))
 console.log(Math.floor(1+Math.random()*6))
 console.log(Math.floor(1+Math.random()*6))
+console.log(Math.floor(1+Math.random()*3))
